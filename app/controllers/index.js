5373314d61b5f68411d7420f3581c8a266a7db27
macielsousa <fmacielsousa@gmail.com>
2021-10-25 12:00:16 -0300

    fix: cadastro de usuario completo

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,18 @@
 module.exports.home = function(application, req, res){
     res.render('index');
 }
+module.exports.autenticar = function(application, req, res){
+    const dadosFomr = req.body;
+
+    req.assert('usuario', 'Usuários deve ser vazio').notEmpty();
+    req.assert('senha', 'Senha não deve ser vazia').notEmpty();
+
+    const erros = req.validationErrors();
+
+    if(erros){
+        res.render('index', {validacao:erros});
+        return;
+    }
+
+    res.send('tuso ok para criar a sessão!');
+}
