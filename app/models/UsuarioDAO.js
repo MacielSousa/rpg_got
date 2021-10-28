77f38e018ec0cd56d3c09071c6cd45a455e37f7b
macielsousa <fmacielsousa@gmail.com>
2021-10-28 16:50:42 -0300

    fix: senha criptografada

--- app/models/UsuarioDAO.js
+++ app/models/UsuarioDAO.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 function UsuarioDAO(connection){
    this._connection = connection();
 }
@@ -5,8 +6,13 @@ function UsuarioDAO(connection){
 UsuarioDAO.prototype.inserirUsuario = function(usuario, res){
     this._connection.open(function(err, mongoclient){
         mongoclient.collection("usuarios", function(err, collection){
+            
+            const senha_criptografada = crypto.createHash("md5").update(usuario.senha).digest("hex");
+            usuario.senha = senha_criptografada;
+            console.log(senha_criptografada)
             collection.insert(usuario);
             mongoclient.close();
+
         });
     });
 }
@@ -14,6 +20,10 @@ UsuarioDAO.prototype.inserirUsuario = function(usuario, res){
 UsuarioDAO.prototype.autenticar = function(usuario, req, res){
     this._connection.open(function(err, mongoclient){
         mongoclient.collection("usuarios", function(err, collection){
+
+            const senha_criptografada = crypto.createHash("md5").update(usuario.senha).digest("hex");
+            usuario.senha = senha_criptografada;
+
             collection.find(usuario).toArray(function(err, result){
                 if(result[0] != undefined){
                     req.session.autorizado = true;
